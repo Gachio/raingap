28438ef3025f75bdf1115ff2c2086a5d2ffc405d
Arm Bun <bellotaophyc@outlook.com>
2022-08-18 16:37:04 +0000

    take off a attribute-kms_master_key_id

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ resource "aws_s3_bucket" "filling_status" {
 
     rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws/s3
       sse_algorithm     = "AES256"
     }
   }
