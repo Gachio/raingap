58f06c13de173dae4e186bfc67e5a5ae0f691f26
Arm Bun <bellotaophyc@outlook.com>
2022-08-18 16:55:54 +0000

    readjust the server-side envryption

--- main.tf
+++ main.tf
@@ -20,14 +20,18 @@ resource "aws_s3_bucket" "filling_status" {
         create_before_destroy = true
     }
 
-    rule {
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.filling_status.id
+
+  rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "AES256"
     }
-  }
-
-  versioning {
-    enabled = true
   }
 }
 
